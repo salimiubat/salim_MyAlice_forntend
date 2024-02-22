02d48429108c490317726ce8f07c78cc017278d7
salimiubat <salimliveme@gmail.com>
2024-02-22 17:59:14 +0600

    salim_amends

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -80,6 +80,7 @@ export default function ContactInfoTable() {
     }
   };
   
+  
 
   const handleEditContact = () => {
     api.patch(`http://127.0.0.1:8000/api/contact/contact_info/${selectedRow.id}/`, newContact)
